272ca980f52d81362c23d5692e8fadbdc08f4486
Jonathan Traverso <jtraverso7@gmail.com>
2019-08-11 14:46:38 -0400

    MVP complete.

--- cars/carsDB.js
+++ cars/carsDB.js
@@ -2,9 +2,25 @@ const express = require("express");
 const DB = require("../configDB.js");
 
 module.exports = {
-  get
+  get,
+  getById,
+  insert
 };
 
 function get() {
   return DB("cars");
+}
+
+function getById(id) {
+  return DB("cars")
+    .where({ id })
+    .first();
+}
+
+function insert(body) {
+  return DB("cars")
+    .insert(body)
+    .then(ids => {
+      getById(ids[0]);
+    });
 }
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -13,4 +13,23 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  carsDB
+    .getById(id)
+    .then(response => {
+      res.status(200).json({ success: true, response });
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+  carsDB.insert(body).then(response => {
+    res.status(201).json({ success: true, response });
+  });
+});
+
 module.exports = router;
